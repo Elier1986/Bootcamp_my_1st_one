c81ffc8728f4e9a4e521cf1fa911398c490d4a09
Elier Trujillo <eliertrujillo1986@gmail.com>
2022-03-31 23:41:00 +0200

    Comienzo a trabajar

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import Message from './Message';
 
-function App() {
+
+const Mensaje = () => {
+  return (
+  <div>
+    <p>Esto es un mensaje desde mensaje.</p>
+  </div>
+  )
+
+}
+
+const Cuerpo =()=> {
+  return(
+    <Message/>
+  )
+}
+
+const Frame = () => {
+  return (
+    <div>
+      <Cuerpo/>
+      <br/>
+      <Mensaje></Mensaje>
+    </div>
+    
+
+  )
+}
+
+
+const  App=() => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 color="red">Esto esta chupao!!</h1>
+      <h2>Estoy trabajando en ello.</h2>
+      <p>Esto esta enredado un poquito. Pero lo <strong>estoy aprendiendo bien.</strong></p>
+      <br/>
+      <Mensaje/>
+      <br></br>
+      <Frame/>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ part1/src/Message.js
@@ -0,0 +1,11 @@
+const Message = () => {
+    return (
+    <div>
+      <p>Esto es un mensaje desde el modulo</p>
+    </div>
+    )
+  
+  }
+  
+  export default Message
+  
